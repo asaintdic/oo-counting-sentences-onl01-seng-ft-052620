567ab89fe279ec231608ad9626f39bd2530759e9
Alain Saint-Dic <alains214@gmail.com>
2020-06-06 18:31:59 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
+    sentences = self.gsub(".", 142).gsub("?", 142).gsub("!", 142).reject {|sentence| == ""}
+    sentences.count
+  end 
 
   end
-end
